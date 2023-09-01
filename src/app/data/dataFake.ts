27cb6f7ac51ac015d66e8c227a05abbaeafa5941
Alderj <33815682+Alderj@users.noreply.github.com>
2023-08-31 21:54:05 -0400

    colocando dados do DataFake dinamico

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,14 @@
 export const dataFake = [
     {
-        "id": 1,
+        "id": "1",
         "photo": "Noticia 1",
         "title":"SAaiu uima nova noticia",
         "description": "descrição da noticia"
     },
     {
-        "id": 2,
-        "photo": "aaa",
+        "id": "2",
+        "photo": "https://s2-g1.glbimg.com/jfVlBdekH1lJIMU7HEK12dBVNms=/0x0:804x479/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/R/F/Vt2xxQSfyFPIk6RCwMdw/captura-de-tela-2023-08-25-104831.png",
         "title":"Noticia 2",
-        "description": "descrição da noticia"
+        "description": "A obrigatoriedade da emissão da Nota Fiscal Eletrônica de Serviços (NFS-e) pelos microempreendedores individuais (MEIs) foi adiado pelo Comitê Gestor do Simples Nacional (CGSN). Previsto inicialmente para começar nesta segunda, 3, os MEIs serão obrigados a emitir a nota a partir do dia 1º de setembro.Além de promover uma padronização, a NFS-e Nacional resolve o impasse de cidades que não recolhem imposto pela ausência de administração tributária ou recursos tecnológicos. http://normas.receita.fazenda.gov.br/sijut2consulta/link.action?idAto=125242`"
     }
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,24 +1,46 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from 'src/app/data/dataFake'
+
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css']
 })
-export class ContentComponent {
+export class ContentComponent implements OnInit {
 
   @Input()
-  photoContent:string = "https://s2-g1.glbimg.com/jfVlBdekH1lJIMU7HEK12dBVNms=/0x0:804x479/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/R/F/Vt2xxQSfyFPIk6RCwMdw/captura-de-tela-2023-08-25-104831.png"
-  @Input()
-  contentTitle:string = "Meu Artigo"
-  @Input()
-  contentDescription:string = `A obrigatoriedade da emissão da Nota Fiscal Eletrônica de Serviços (NFS-e) pelos microempreendedores individuais (MEIs) foi adiado pelo Comitê Gestor do Simples Nacional (CGSN). Previsto inicialmente para começar nesta segunda, 3, os MEIs serão obrigados a emitir a nota a partir do dia 1º de setembro.
+  photoContent:string = ""
 
-  Além de promover uma padronização, a NFS-e Nacional resolve o impasse de cidades que não recolhem imposto pela ausência de administração tributária ou recursos tecnológicos.
+  @Input()
+  contentTitle:string = ""
+
+  @Input()
+  contentDescription:string = ""
+
+  private id:string | null = "0"
+
+  constructor(
+    private route:ActivatedRoute
+  ){ }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe( value =>
+      this.id = value.get("id")     
+    )
+
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id:string | null){
+    const result = dataFake.filter(
+      article => article.id == id) [0]
   
-  
-  http://normas.receita.fazenda.gov.br/sijut2consulta/link.action?idAto=125242
-
-  `
-
+      this.contentTitle = result.title
+      this.contentDescription = result.description
+      this.photoContent = result.photo
+      
+    
+  }
 }
