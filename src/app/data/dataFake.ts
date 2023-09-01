ede20e212dc20727bb32e3057fc64af601a211b1
Alderj <33815682+Alderj@users.noreply.github.com>
2023-08-31 22:43:15 -0400

    ajustando arquivos .css

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -4,13 +4,24 @@ export const dataFake = [
         "id": "1",
         "photo": "https://s2-g1.glbimg.com/jfVlBdekH1lJIMU7HEK12dBVNms=/0x0:804x479/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/R/F/Vt2xxQSfyFPIk6RCwMdw/captura-de-tela-2023-08-25-104831.png",
 
-        "title":"NFSE deve ser emitida apenas pelo portal do Governo Federal",
+        "title":"MEI deve emitir NFS-e apenas pelo portal do Governo Federal",
 
-        "description": "A obrigatoriedade da emissão da Nota Fiscal Eletrônica de Serviços (NFS-e) pelos microempreendedores individuais (MEIs) foi adiado pelo Comitê Gestor do Simples Nacional (CGSN). Previsto inicialmente para começar nesta segunda, 3, os MEIs serão obrigados a emitir a nota a partir do dia 1º de setembro.Além de promover uma padronização, a NFS-e Nacional resolve o impasse de cidades que não recolhem imposto pela ausência de administração tributária ou recursos tecnológicos. http://normas.receita.fazenda.gov.br/sijut2consulta/link.action?idAto=125242`"
+        "description": "A partir de 1º de setembro de 2023, todo MEI (Micro Empreendedor Individual), deverá realizar todas as emissões das suas Notas Fiscais de Serviço Eletrônicas - NFS-e, através do portal do Governo Federal. Segue o link da RESOLUÇÃO CGSN Nº 169, DE 27 DE JULHO DE 2022  http://normas.receita.fazenda.gov.br/sijut2consulta/link.action?idAto=125242"
     },
     {
         "id": "2",
+
         "photo": "https://www.gov.br/esocial/pt-br/noticias/disponibilizadas-videoaulas-sobre-a-escrituracao-dos-processos-trabalhistas-no-esocial/esocial.png/@@images/24e5c045-e0f7-4996-bcca-1078d562c365.png",
+
+        "title": "Cursos Online Gratuitos",
+        
+        "description": "A ENIT - Escola Nacional da Inspeção do Trabalho, ligada à Secretaria de Inspeção do Trabalho, órgão do Ministério do Trabalho e Emprego, lançou um curso online gratuito sobre os eventos de processos trabalhistas no eSocial. O curso, que traz orientações detalhadas, será implantado em duas etapas: a primeira etapa, já disponibilizada, trata da escrituração do evento S-2500 (Processo Trabalhista); a segunda etapa, a ser disponibilizada em meados de setembro/23, abordará a escrituração dos eventos S-2501 (Informações de Tributos Decorrentes de Processo Trabalhista) e S-3500 (Exclusão de Eventos - Processo Trabalhista).São vídeos curtos e, por serem apresentados em módulos, fica fácil a localização do assunto de interesse. O curso dará direito a certificado de conclusão, atendidas as condições previstas. O curso Processos Trabalhistas no eSocial está disponível na página da ENIT (https://ead.sit.trabalho.gov.br/)."
+    },
+    {
+        "id": "3",
+
+        "photo": "https://www.gov.br/esocial/pt-br/noticias/disponibilizadas-videoaulas-sobre-a-escrituracao-dos-processos-trabalhistas-no-esocial/esocial.png/@@images/24e5c045-e0f7-4996-bcca-1078d562c365.png",
+        
         "title": "Cursos Online Gratuitos",
         
         "description": "A ENIT - Escola Nacional da Inspeção do Trabalho, ligada à Secretaria de Inspeção do Trabalho, órgão do Ministério do Trabalho e Emprego, lançou um curso online gratuito sobre os eventos de processos trabalhistas no eSocial. O curso, que traz orientações detalhadas, será implantado em duas etapas: a primeira etapa, já disponibilizada, trata da escrituração do evento S-2500 (Processo Trabalhista); a segunda etapa, a ser disponibilizada em meados de setembro/23, abordará a escrituração dos eventos S-2501 (Informações de Tributos Decorrentes de Processo Trabalhista) e S-3500 (Exclusão de Eventos - Processo Trabalhista).São vídeos curtos e, por serem apresentados em módulos, fica fácil a localização do assunto de interesse. O curso dará direito a certificado de conclusão, atendidas as condições previstas. O curso Processos Trabalhistas no eSocial está disponível na página da ENIT (https://ead.sit.trabalho.gov.br/)."
