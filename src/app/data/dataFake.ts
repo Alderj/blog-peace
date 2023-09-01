04ce02631d14abd77ea0e418895eed7aaa5f4f03
Alderj <33815682+Alderj@users.noreply.github.com>
2023-08-31 21:04:11 -0400

    criado um dataFake

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,32 @@
+export const dataFake = [
+    {
+        "id": 1,
+        "photo": "Noticia 1",
+        "title":"SAaiu uima nova noticia",
+        "description": "descrição da noticia"
+    },
+    {
+        "id": 2,
+        "photo": "aaa",
+        "title":"Noticia 2",
+        "description": "descrição da noticia"
+    },
+    {
+        "id": 3,
+        "photo": "aaa",
+        "title":"Noticia 3",
+        "description": "descrição da noticia"
+    },
+    {
+        "id": 4,
+        "photo": "aaa",
+        "title":"Noticia 4",
+        "description": "descrição da noticia"
+    },
+    {
+        "id": 5,
+        "photo": "aaa",
+        "title":"Noticia 5",
+        "description": "descrição da noticia"
+    },
+]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-content',
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent {
+
+  @Input()
+  photoContent:string = "https://s2-g1.glbimg.com/jfVlBdekH1lJIMU7HEK12dBVNms=/0x0:804x479/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/R/F/Vt2xxQSfyFPIk6RCwMdw/captura-de-tela-2023-08-25-104831.png"
+  @Input()
+  contentTitle:string = "Meu Artigo"
+  @Input()
+  contentDescription:string = `A obrigatoriedade da emissão da Nota Fiscal Eletrônica de Serviços (NFS-e) pelos microempreendedores individuais (MEIs) foi adiado pelo Comitê Gestor do Simples Nacional (CGSN). Previsto inicialmente para começar nesta segunda, 3, os MEIs serão obrigados a emitir a nota a partir do dia 1º de setembro.
+
+  Além de promover uma padronização, a NFS-e Nacional resolve o impasse de cidades que não recolhem imposto pela ausência de administração tributária ou recursos tecnológicos.
+  
+  
+  http://normas.receita.fazenda.gov.br/sijut2consulta/link.action?idAto=125242
+
+  `
 
 }
